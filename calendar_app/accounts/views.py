fbbacd2dead327363f2bc9dc18a3a9bd3b4794c8
chondosha1234 <jmiller61193@gmail.com>
2022-12-21 15:29:37 -0500

    created accounts app and supporting folders

--- calendar_app/accounts/views.py
+++ calendar_app/accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.contrib.auth import authenticate, login, logout
+from accounts.forms import SignInForm, SignUpForm
 # Create your views here.
+
+class SignInView(View):
+    template_name = "signin.html"
+    form_class = SignInForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            email = form.cleaned_date["email"]
+            password = form.cleaned_date["password"]
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect("cal:calendar")
+        context = {
+            "form": form
+        }
+        return render(request, self.template_name, context)
+
+
+def signout(request):
+    logout(request)
+    return redirect("signin")
+
+
+class SignUpView(View):
+    template_name = "signup.html"
+    form_class = SignUpForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("signin")
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
