d685a7d925b73e5223df161f6b7809cda7e2a55b
chondosha1234 <jmiller61193@gmail.com>
2022-12-23 11:51:09 -0500

    added namespaces to urls and html

--- calendar_app/cal/urls.py
+++ calendar_app/cal/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "cal"
+
 urlpatterns = [
     path('', views.CalendarView.as_view(), name='calendar'),
     path('event/new/', views.event_new, name='event_new'),
