9669446eba3859599d54dc2c27840ad6160b469b
chondosha1234 <jmiller61193@gmail.com>
2022-12-27 12:41:17 -0500

    removed models file and moved to models folder

---
+++ calendar_app/cal/models/event.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+from datetime import datetime
+from accounts.models import User
+
+
+# Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('cal:event_details', args=[self.id])
+
+    @property
+    def get_html_url(self):
+        url = reverse('cal:event_details', args=[self.id])
+        return f'<a class="event-link" href="{url}">{self.title}</a>'
